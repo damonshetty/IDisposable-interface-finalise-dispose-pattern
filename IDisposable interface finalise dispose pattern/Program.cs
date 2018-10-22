80c39c9489146d661cb6bcf408e68e05527792a1
Damon Shetty <damon_53@hotmail.com>
2018-10-22 14:56:30 +0100

    Commit

---
+++ IDisposable interface finalise dispose pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposable_interface_finalise_dispose_pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class cls1 : IDisposable
+    {
+        ~cls1()
+        {
+
+        }
+        
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+    }
+}
